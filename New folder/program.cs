a2d631481129a39c199ae4f6af75262fbc3df76c
stalc0n <playn1212@gmail.com>
2023-09-10 18:28:26 +0300

    adding cs script

---
+++ New folder/program.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Ivgi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shlomo");
+        }
+
+        //private static byte[] key = new byte[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        //private static byte[] iv = new byte[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+    }
+}
